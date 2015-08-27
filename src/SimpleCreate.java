1f68f85e3a44f9ceee594aff07aac3203619edf9
sdanilov <sdanilov@kirkazan.ru>
2015-08-27 18:53:29 +0300

    [nf] adding dataset, label

--- src/SimpleCreate.java
+++ src/SimpleCreate.java
@@ -26,6 +26,12 @@ public class SimpleCreate
         }
     }
 
+    static LabelHandle createLabel(ElementFactory efactory, String name, String text) throws SemanticException {
+        LabelHandle l = efactory.newLabel(name);
+        l.setText(text);
+        return l;
+    }
+
     // This method shows how to build a very simple BIRT report with a
     // minimal set of content: a simple grid with an image and a label.
 
@@ -56,17 +62,17 @@ public class SimpleCreate
         ReportDesignHandle design = session.createDesign( );
 
         // The element factory creates instances of the various BIRT elements.
-        ElementFactory efactory = design.getElementFactory( );
+        ElementFactory eFactory = design.getElementFactory( );
 
         // Create a simple master page that describes how the report will appear when printed.
         //
         // Note: The report will fail to load in the BIRT designer unless you create a master page.
-        DesignElementHandle element = efactory.newSimpleMasterPage( "Page Master" );
+        DesignElementHandle element = eFactory.newSimpleMasterPage( "Page Master" );
         design.getMasterPages( ).add( element );
 
         // Create a grid and add it to the "body" slot of the report design.
-        GridHandle grid = efactory.newGridItem( null, 2 /* cols */, 1 /* row */ );
-        design.getBody( ).add( grid );
+        GridHandle grid = eFactory.newGridItem( null, 2 /* cols */, 1 /* row */ );
+        design.getBody( ).add(grid);
 
         // Note: Set the table width to 100% to prevent the label
         // from appearing too narrow in the layout view.
@@ -76,32 +82,50 @@ public class SimpleCreate
         RowHandle row = (RowHandle) grid.getRows( ).get( 0 );
 
         // Create an image and add it to the first cell.
-        ImageHandle image = efactory.newImage( null );
+        ImageHandle image = eFactory.newImage( null );
         CellHandle cell = (CellHandle) row.getCells( ).get( 0 );
-        cell.getContent( ).add( image );
+        cell.getContent( ).add(image);
 //        image.setURL( "\"urlofimage\"" );
 
         // Create a label and add it to the second cell.
-        LabelHandle label = efactory.newLabel( null );
+        LabelHandle label = eFactory.newLabel( null );
         cell = (CellHandle) row.getCells( ).get( 1 );
         cell.getContent( ).add( label );
-        label.setText( "Hello, world!" );
+        label.setText("Hello, world!");
 
 
         // Adding DataSource
-        OdaDataSourceHandle dsHandle = efactory.newOdaDataSource("DS", "org.eclipse.birt.report.data.oda.jdbc");
-        dsHandle.setProperty( "odaDriverClass",
-                "org.postgresql.Driver" );
-        dsHandle.setProperty( "odaURL", "jdbc:postgresql://127.0.0.1:6532/iis" );
-        dsHandle.setProperty( "odaUser", "postgres" );
-        dsHandle.setProperty( "odaPassword", "postgres" );
+        OdaDataSourceHandle dataSource = eFactory.newOdaDataSource("DS", "org.eclipse.birt.report.data.oda.jdbc");
+        dataSource.setProperty("odaDriverClass", "org.postgresql.Driver");
+        dataSource.setProperty("odaURL", "jdbc:postgresql://127.0.0.1:6532/iis");
+        dataSource.setProperty("odaUser", "postgres");
+        dataSource.setProperty("odaPassword", "postgres");
+        design.getDataSources().add(dataSource);
 
-        design.getDataSources( ).add( dsHandle );
+        // Adding DataSet
+        OdaDataSetHandle dataSet = eFactory.newOdaDataSet("ds", "org.eclipse.birt.report.data.oda.jdbc.JdbcSelectDataSet");
+        dataSet.setDataSource("DS");
+        String qry = "Select * from localization";
+        dataSet.setQueryText(qry);
+        design.getDataSets().add(dataSet);
+
+        // Adding Table
+        TableHandle table = eFactory.newTableItem("table", 3);
+        table.setWidth("100%");
+        table.setDataSet(design.findDataSet("ds"));
+            // header defining
+            RowHandle tableHeader = (RowHandle) table.getHeader().get(0);
+            CellHandle h0 =  (CellHandle) tableHeader.getCells().get(0);
+            h0.getContent().add(createLabel(eFactory, "label1", "Колонка 1"));
+
+        design.getBody().add(table);
+
+
 
 
 
         // Save the design and close it.
-        design.saveAs( "d:/temp/sample1.rptdesign" );
+        design.saveAs( "d:/temp/sample1.rptdesign");
         design.close( );
         System.out.println("Finished");
 
